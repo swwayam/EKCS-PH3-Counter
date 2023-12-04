28fede85817e420c03e58a1773e3280ecc93454c
ekcs-swayam <ssuryavanshi@mediaferry.com>
2023-12-04 21:41:56 +0530

    EKCS_PH

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'counter';
+  count = 1;
+
+  increaseCounter(){
+    this.count++
+  }
+ 
+  decreaseCounter(){
+    if(this.count > 0){
+      this.count--
+    }
+  }
+
+  // When you click on + | it should multiply by 2 
+  // When you click on - | it should divide by 2 
+
 }
