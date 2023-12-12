05096dd2bde5b0c1a09f22609951f904d7226e70
ekcs-swayam <ssuryavanshi@mediaferry.com>
2023-12-12 20:47:43 +0530

    Project Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,23 +12,53 @@ import { RouterOutlet } from '@angular/router';
 
 
 export class AppComponent {
-  count = 1;
+  count = 0;
+  availableItem = 10;
 
   increaseCounter(){
+
+    // If count exceeds the availableItem give user an popup / error
+    if(this.count < this.availableItem){
+      this.count++
+    }
+    
+    // else{
+    //   alert("Only " + this.availableItem + " available")
+    // }
+
     // this.count++ means ->
     // this.count = this.count + 1
 
     // if you want to multiply you can't say this.count*2
-    this.count = this.count * 2
+    // this.count = this.count * 2
   }
  
   decreaseCounter(){
     if(this.count > 0){
       this.count--
     }
+  
   }
+
+  /***
+   * 
+   * HW
+   * 
+   */
 
   // When you click on + | it should multiply by 2 
   // When you click on - | it should divide by 2 
 
+
+  /*** 5th Dec 23 */
+  // There should be a buy button in html
+  // if user clicks on buy the availabeItems should be updated
+  // cart / count should become zero
+
+  purchase(){
+                         // 10              -     3
+    this.availableItem = this.availableItem - this.count
+    this.count = 0
+  }
+  
 }
